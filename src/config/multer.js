93c236b6bfc9d4b055ae9a90245d76a83b184a34
jozimar <jozimarjo@gmail.com>
2020-02-03 16:56:26 -0400

    criando migration de file

--- src/config/multer.js
+++ src/config/multer.js
@@ -6,7 +6,6 @@ export default {
     storage: multer.diskStorage({
         destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'), // destino dos arquivos
         filename: (req, file, cb) => {
-            console.log('multer = ', req.body);
             // file name como iremos formatar o nome do arquivo da imagem.
             crypto.randomBytes(16, (err, res) => {
                 if (err) return cb(err);
