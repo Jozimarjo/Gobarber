b2508f5c476e84f2f1bc21fd6f36a7044a788da8
jozimar <jozimarjo@gmail.com>
2020-02-04 17:00:23 -0400

    AppointmentController

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -3,7 +3,7 @@ import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
-
+// Usuario comum --> nao prestador de serviço
 class AppointmentController {
     async index(req, res) {
         const { page = 1 } = req.query;
@@ -44,7 +44,7 @@ class AppointmentController {
             return res.status(400).json({ error: 'Validation fails' });
 
         const { provider_id, date } = req.body;
-
+        console.log('CHEGAMOS NO STORE ', date, ' --- ', provider_id);
         /**
          * Checar se o usario e um provedor de Serviço
          */
@@ -59,6 +59,7 @@ class AppointmentController {
          * Checando se a data e superior a atual
          */
         const hourStart = startOfHour(parseISO(date));
+
         if (isBefore(hourStart, new Date()))
             return res.status(400).json({
                 error: 'Data invalida',
@@ -78,6 +79,7 @@ class AppointmentController {
             return res.status(400).json({
                 error: 'Data indisponivel ',
             });
+        console.log('CHEGAMOS NO STORE ', hourStart, ' --- ', provider_id);
 
         const appointment = await Appointment.create({
             user_id: req.userId,
